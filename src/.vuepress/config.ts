cae0f8d5b92e4c02e96ff7f8c09cea67881e76d9
tangxi <13057252189@163.com>
2025-03-30 23:52:00 +0800

    feat:项目配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "寻己小习",
+  description: "寻己小习",
 
   theme,
 
---
+++ src/home/home.md
@@ -0,0 +1,11 @@
+---
+title: 总览
+layout: BlogHome
+icon: house
+hero: false
+heroImage: /logo.svg
+heroText: 寻己小习
+heroFullScreen: true
+tagline: 简单 自由 纯粹
+footer: 简单 自由 纯粹
+---
